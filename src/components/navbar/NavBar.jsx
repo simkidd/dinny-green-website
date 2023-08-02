22fd776b1cb069763911acbbc61c0a4540787865
unknown <johnmsn22@gmail.com>
2023-08-02 11:36:13 +0100

    init responsiveness

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/Group_60.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-inner-container">
@@ -13,14 +20,32 @@ const NavBar = () => {
           </Link>
         </div>
 
-        <ul className="navlinks">
-          <li><Link>Home</Link></li>
-          <li><Link>About</Link></li>
-          <li><Link>Services</Link></li>
-          <li><Link>Instructors</Link></li>
-          <li><Link>Courses</Link></li>
-          <li><Link>Blog</Link></li>
-          <li><Link className="contact">Contact</Link></li>
+        <div className="menu-btn" onClick={handleMenu}>
+          {showMenu ? <FaTimes/> : <FaBars />}
+        </div>
+
+        <ul className={showMenu ? "mobile-navlinks" : "navlinks"}>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link>About</Link>
+          </li>
+          <li>
+            <Link>Services</Link>
+          </li>
+          <li>
+            <Link>Instructors</Link>
+          </li>
+          <li>
+            <Link>Courses</Link>
+          </li>
+          <li>
+            <Link>Blog</Link>
+          </li>
+          <li>
+            <Link className="contact">Contact</Link>
+          </li>
         </ul>
       </div>
     </div>
